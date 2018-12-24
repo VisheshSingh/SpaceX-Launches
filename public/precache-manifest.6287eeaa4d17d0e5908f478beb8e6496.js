b6ed8533fc1e35a61bb763f542f4cb4d06a47dce
Vishesh <visheshsingh16@gmail.com>
2018-12-24 13:14:27 -0500

    Heroku deploy to prod

---
+++ public/precache-manifest.6287eeaa4d17d0e5908f478beb8e6496.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "adbf010c1848f3cdd9a60d94930b7b9a",
+    "url": "/static/media/logo.adbf010c.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "426dd8c5db3be2e382a4",
+    "url": "/static/js/main.426dd8c5.chunk.js"
+  },
+  {
+    "revision": "74448b3e6e951fad4308",
+    "url": "/static/js/1.74448b3e.chunk.js"
+  },
+  {
+    "revision": "426dd8c5db3be2e382a4",
+    "url": "/static/css/main.7c9faef3.chunk.css"
+  },
+  {
+    "revision": "87d8134dcedc4d8d756f7b462cabe004",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const graphQLHTTP = require("express-graphql");
 const schema = require("./schema");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
@@ -14,6 +15,10 @@ app.use(
   })
 );
 
-const PORT = process.env.port || 5000;
+app.use(express.static("public"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server listening to ${PORT}...`));
